ad6986b66e1646e8bc769a273fd7e6c18662a47e
Gian Tomas Lacey <glacey@ssn.gob.ar>
2023-11-17 17:21:23 -0300

    Todos los autos y un filtro

--- javascript/index.js
+++ javascript/index.js
@@ -132,3 +132,95 @@ function logout() {
 
 
 }
+
+
+
+function CarForRent(brand, model, price, availability, img, type) {
+    this.brand = brand;
+    this.model = model;
+    this.price = price;
+    this.availability = availability;
+    this.img = img;
+    this.type = type;
+}
+
+//Chevrolete Car
+const car1 = new CarForRent('Chevrolet', 'S10 Double Cabin', 200, true, './assest/imagenes/rental_car/cvrlt/cab-dupla.webp', 'van');
+
+const car2 = new CarForRent('Chevrolet', 'S10 Simple Cabin', 200, true, './assest/imagenes/rental_car/cvrlt/cab-simples.webp', 'van');
+
+const car3 = new CarForRent('Chevrolet', 'Camaro Cupe', 300, true, './assest/imagenes/rental_car/cvrlt/camaro-interior1.png', 'sport-car');
+
+const car4 = new CarForRent('Chevrolet', 'Tahoe', 200, true, './assest/imagenes/rental_car/cvrlt/CHEVROLET_TAHOE_2016.webp', 'family-van');
+
+const car5 = new CarForRent('Chevrolet', 'Cruze Final', 100, true, './assest/imagenes/rental_car/cvrlt/chevrolet-cruze-final.webp', 'family-car');
+
+const car6 = new CarForRent('Chevrolet', 'Corsa Classic', 100, true, './assest/imagenes/rental_car/cvrlt/corsa-classic.webp', 'family-car');
+
+const car7 = new CarForRent('Chevrolet', 'Camaro Cupe', 300, true, './assest/imagenes/rental_car/cvrlt/novo-camaro-cupe.webp', 'sport-car');
+
+const car8 = new CarForRent('Ford', 'Expedition', 100, true, './assest/imagenes/rental_car/ford/ford-expedition.webp', 'family-car');
+
+const car9 = new CarForRent('Ford', 'Focus', 200, true, './assest/imagenes/rental_car/ford/ford-focus.jpg', 'family-van');
+
+const car10 = new CarForRent('Ford', 'Mustang', 300, true, './assest/imagenes/rental_car/ford/ford-mustang.webp', 'sport-car');
+
+const car11 = new CarForRent('Ford', 'Mustang', 300, true, './assest/imagenes/rental_car/ford/ford-mustang-r.webp', 'sport-car');
+
+const car12 = new CarForRent('Ford', 'Ranger', 200, true, './assest/imagenes/rental_car/ford/ford-ranger.png', 'van');
+
+const car13 = new CarForRent('Ford', 'Ranger Simple Cabin', 200, true, './assest/imagenes/rental_car/ford/Ford-Ranger-Cabina-Simple.jpg', 'van');
+
+const car14 = new CarForRent('Ford', 'Ranger Raptor', 300, true, './assest/imagenes/rental_car/ford/Ford-Ranger-Raptor.jpg', 'van');
+
+const car15 = new CarForRent('Volkswagen', 'Amarok', 200, true, './assest/imagenes/rental_car/vkwgn/amarok.png', 'van');
+
+const car16 = new CarForRent('Volkswagen', 'Nivus', 200, true, './assest/imagenes/rental_car/vkwgn/volk-nivus.webp', 'family-van');
+
+const car17 = new CarForRent('Volkswagen', 'Polo', 100, true, './assest/imagenes/rental_car/vkwgn/vw-polo.jpg', 'family-car');
+
+const car18 = new CarForRent('Volkswagen', 'Sciroco', 300, true, './assest/imagenes/rental_car/vkwgn/vw-sciroco.webp', 'sport-car');
+
+const car19 = new CarForRent('Volkswagen', 'Tiguan', 200, true, './assest/imagenes/rental_car/vkwgn/vw-tiguan.jpg', 'family-van');
+
+const car20 = new CarForRent('Volkswagen', 'Vento', 100, true, './assest/imagenes/rental_car/vkwgn/vw-vento.png', 'family-car');
+
+
+const carsArray = [car1, car2, car3, car4, car5, car6, car7, car8, car9, car10, car11, car12, car13, car14, car15, car16, car17, car18, car19, car20];
+
+const carsArrayJSON = JSON.stringify(carsArray);
+
+localStorage.setItem('carsArray', carsArrayJSON);
+
+const storedCarsArrayJSON = localStorage.getItem('carsArray');
+const storedCarsArray = JSON.parse(storedCarsArrayJSON);
+
+const container = document.getElementById('carsForRent');
+
+storedCarsArray.forEach(car => {
+    const carElement = document.createElement('div');
+    carElement.classList.add("carCont");
+    carElement.innerHTML = `<div class="img-cont"><img src="${car.img}"                        alt="${car.brand} ${car.model}"></div>
+                            <p><b> ${car.brand}</b></p>
+                            <p> ${car.model}</p>
+                            <p>Price: $${car.price} /Day</p>
+                            <p> ${car.availability ? 'Available' : 'Not Available'}</p>
+                            <button type="button" class="rentNow">Rent Now</button>
+                            `;
+    container.appendChild(carElement);
+});
+
+//FILTRO ASIDE
+// Obtiene todos los elementos con la clase 'checkbox'
+let checkboxes = document.querySelectorAll('.checkbox');
+
+// Agrega un listener a cada checkbox
+checkboxes.forEach(function(checkbox) {
+    checkbox.addEventListener('click', function() {
+        this.classList.toggle('checked'); 
+    });
+});
+
+
+
+
