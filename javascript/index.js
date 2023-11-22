2a19718f9c1c81d71337d8a5a02efc2b0b251263
Gian Tomas Lacey <glacey@ssn.gob.ar>
2023-11-22 14:26:53 -0300

    Ultimo cambios

--- javascript/index.js
+++ javascript/index.js
@@ -1,3 +1,6 @@
+
+
+
 function showForm(formId){
     let formCont = document.getElementById("formContainer");
 
@@ -63,11 +66,11 @@ function register() {
             let loggedInUser = document.getElementById('loggedInUser');
             let userLogged = document.getElementById('userLogged');
             let usersLogReg = document.getElementById('usersLogReg');
-            
+            let formContainer = document.getElementById('formContainer');
 
             loggedInUser.style.display = "flex";
             usersLogReg.style.display = "none";
-            formCont.style.display = "none";
+            formContainer.style.display = "none";
 
             userLogged.innerHTML = username.charAt(0).toUpperCase() + username.slice(1).toLowerCase();
 
@@ -174,101 +177,18 @@ function logout() {
 }
 
 
-
-function carForRent(brand, model, price, availability, img, type, id) {
-    this.brand = brand;
-    this.model = model;
-    this.price = price;
-    this.availability = availability;
-    this.img = img;
-    this.type = type;
-    this.id = id;
-}
-
-//Chevrolete Car
-const car1 = new carForRent('Chevrolet', 'S10 Double Cabin', 200, true, '../assest/imagenes/rental_car/cvrlt/cab-dupla.webp', 'van', 1);
-
-const car2 = new carForRent('Chevrolet', 'S10 Simple Cabin', 200, true, '../assest/imagenes/rental_car/cvrlt/cab-simples.webp', 'van', 2);
-
-const car3 = new carForRent('Chevrolet', 'Camaro Cupe', 300, true, '../assest/imagenes/rental_car/cvrlt/camaro-interior1.png', 'sport-car', 3);
-
-const car4 = new carForRent('Chevrolet', 'Tahoe', 200, true, '../assest/imagenes/rental_car/cvrlt/CHEVROLET_TAHOE_2016.webp', 'family-van', 4);
-
-const car5 = new carForRent('Chevrolet', 'Cruze Final', 100, true, '../assest/imagenes/rental_car/cvrlt/chevrolet-cruze-final.webp', 'family-car', 5);
-
-const car6 = new carForRent('Chevrolet', 'Corsa Classic', 100, true, '../assest/imagenes/rental_car/cvrlt/corsa-classic.webp', 'family-car', 6);
-
-const car7 = new carForRent('Chevrolet', 'Camaro Cupe', 300, true, '../assest/imagenes/rental_car/cvrlt/novo-camaro-cupe.webp', 'sport-car', 7);
-
-const car8 = new carForRent('Ford', 'Expedition', 100, true, '../assest/imagenes/rental_car/ford/ford-expedition.webp', 'family-car', 8);
-
-const car9 = new carForRent('Ford', 'Focus', 200, true, '../assest/imagenes/rental_car/ford/ford-focus.jpg', 'family-van', 9);
-
-const car10 = new carForRent('Ford', 'Mustang', 300, true, '../assest/imagenes/rental_car/ford/ford-mustang.webp', 'sport-car', 10);
-
-const car11 = new carForRent('Ford', 'Mustang', 300, true, '../assest/imagenes/rental_car/ford/ford-mustang-r.webp', 'sport-car', 11);
-
-const car12 = new carForRent('Ford', 'Ranger', 200, true, '../assest/imagenes/rental_car/ford/ford-ranger.png', 'van', 12);
-
-const car13 = new carForRent('Ford', 'Ranger Simple Cabin', 200, true, '../assest/imagenes/rental_car/ford/Ford-Ranger-Cabina-Simple.jpg', 'van', 13);
-
-const car14 = new carForRent('Ford', 'Ranger Raptor', 300, true, '../assest/imagenes/rental_car/ford/Ford-Ranger-Raptor.jpg', 'van', 14);
-
-const car15 = new carForRent('Volkswagen', 'Amarok', 200, true, '../assest/imagenes/rental_car/vkwgn/amarok.png', 'van', 15);
-
-const car16 = new carForRent('Volkswagen', 'Nivus', 200, true, '../assest/imagenes/rental_car/vkwgn/volk-nivus.webp', 'family-van', 16);
-
-const car17 = new carForRent('Volkswagen', 'Polo', 100, true, '../assest/imagenes/rental_car/vkwgn/vw-polo.jpg', 'family-car', 17);
-
-const car18 = new carForRent('Volkswagen', 'Sciroco', 300, true, '../assest/imagenes/rental_car/vkwgn/vw-sciroco.webp', 'sport-car', 18);
-
-const car19 = new carForRent('Volkswagen', 'Tiguan', 200, true, '../assest/imagenes/rental_car/vkwgn/vw-tiguan.jpg', 'family-van', 19);
-
-const car20 = new carForRent('Volkswagen', 'Vento', 100, true, '../assest/imagenes/rental_car/vkwgn/vw-vento.png', 'family-car', 20);
-
-
-const carsArray = [car1, car2, car3, car4, car5, car6, car7, car8, car9, car10, car11, car12, car13, car14, car15, car16, car17, car18, car19, car20];
-
-const carsArrayJSON = JSON.stringify(carsArray);
-
-localStorage.setItem('carsArray', carsArrayJSON);
-
-const storedCarsArrayJSON = localStorage.getItem('carsArray');
-const storedCarsArray = JSON.parse(storedCarsArrayJSON);
-
-const container = document.getElementById('carsForRent');
-
-if(container) {
-    storedCarsArray.forEach(car => {
-        const carElement = document.createElement('div');
-        carElement.classList.add("carCont");
-        carElement.innerHTML = `<div class="img-cont"><img src="${car.img}"                        alt="${car.brand} ${car.model}"></div>
-                                <p><b> ${car.brand}</b></p>
-                                <p> ${car.model}</p>
-                                <p>Price: $${car.price} /Day</p>
-                                <p> ${car.availability ? 'Available' : 'Not Available'}</p>
-                                <button type="button" id="${car.id}" class="rentNow" onclick="rentNow(${car.id})">Rent Now</button>
-                                `;
-        container.appendChild(carElement);
-    });
-}
-
-function rentNow(carId){
-    if(sessionStatus === true){
-        sessionStorage.setItem('selectedCarId', carId);
-        window.location.href = './page/rentNow.html';
-        
-    } else {
-        Swal.fire({
-            icon: 'error',
-            title: 'Log in first',
-        });
-    }
-}
-
 window.onload = function (user) {
-    const savedSessionStatus = sessionStorage.getItem('sessionStatus');
     
+    
+    const savedSessionStatus = sessionStorage.getItem('sessionStatus');
+
+    const storedCarsArrayJSON = sessionStorage.getItem('carsArray') || localStorage.getItem('carsArray');
+
+    
+    const storedCarsArray = JSON.parse(storedCarsArrayJSON);
+    
+
+
     if (savedSessionStatus === 'true') {
         // La sesión está iniciada
         sessionStatus = true;
@@ -330,6 +250,99 @@ window.onload = function (user) {
    
 };
 
+function carForRent(brand, model, price, availability, img, type, id) {
+    this.brand = brand;
+    this.model = model;
+    this.price = price;
+    this.availability = availability;
+    this.img = img;
+    this.type = type;
+    this.id = id;
+}
+
+//Chevrolete Car
+const car1 = new carForRent('Chevrolet', 'S10 Double Cabin', 200, true, '../assest/imagenes/rental_car/cvrlt/cab-dupla.webp', 'van', 1);
+
+const car2 = new carForRent('Chevrolet', 'S10 Simple Cabin', 200, true, '../assest/imagenes/rental_car/cvrlt/cab-simples.webp', 'van', 2);
+
+const car3 = new carForRent('Chevrolet', 'Camaro Cupe', 300, true, '../assest/imagenes/rental_car/cvrlt/camaro-interior1.png', 'sport-car', 3);
+
+const car4 = new carForRent('Chevrolet', 'Tahoe', 200, true, '../assest/imagenes/rental_car/cvrlt/CHEVROLET_TAHOE_2016.webp', 'family-van', 4);
+
+const car5 = new carForRent('Chevrolet', 'Cruze Final', 100, true, '../assest/imagenes/rental_car/cvrlt/chevrolet-cruze-final.webp', 'family-car', 5);
+
+const car6 = new carForRent('Chevrolet', 'Corsa Classic', 100, true, '../assest/imagenes/rental_car/cvrlt/corsa-classic.webp', 'family-car', 6);
+
+const car7 = new carForRent('Chevrolet', 'Camaro Cupe', 300, true, '../assest/imagenes/rental_car/cvrlt/novo-camaro-cupe.webp', 'sport-car', 7);
+
+const car8 = new carForRent('Ford', 'Expedition', 100, true, '../assest/imagenes/rental_car/ford/ford-expedition.webp', 'family-car', 8);
+
+const car9 = new carForRent('Ford', 'Focus', 200, true, '../assest/imagenes/rental_car/ford/ford-focus.jpg', 'family-van', 9);
+
+const car10 = new carForRent('Ford', 'Mustang', 300, true, '../assest/imagenes/rental_car/ford/ford-mustang.webp', 'sport-car', 10);
+
+const car11 = new carForRent('Ford', 'Mustang', 300, true, '../assest/imagenes/rental_car/ford/ford-mustang-r.webp', 'sport-car', 11);
+
+const car12 = new carForRent('Ford', 'Ranger', 200, true, '../assest/imagenes/rental_car/ford/ford-ranger.png', 'van', 12);
+
+const car13 = new carForRent('Ford', 'Ranger Simple Cabin', 200, true, '../assest/imagenes/rental_car/ford/Ford-Ranger-Cabina-Simple.jpg', 'van', 13);
+
+const car14 = new carForRent('Ford', 'Ranger Raptor', 300, true, '../assest/imagenes/rental_car/ford/Ford-Ranger-Raptor.jpg', 'van', 14);
+
+const car15 = new carForRent('Volkswagen', 'Amarok', 200, true, '../assest/imagenes/rental_car/vkwgn/amarok.png', 'van', 15);
+
+const car16 = new carForRent('Volkswagen', 'Nivus', 200, true, '../assest/imagenes/rental_car/vkwgn/volk-nivus.webp', 'family-van', 16);
+
+const car17 = new carForRent('Volkswagen', 'Polo', 100, true, '../assest/imagenes/rental_car/vkwgn/vw-polo.jpg', 'family-car', 17);
+
+const car18 = new carForRent('Volkswagen', 'Sciroco', 300, true, '../assest/imagenes/rental_car/vkwgn/vw-sciroco.webp', 'sport-car', 18);
+
+const car19 = new carForRent('Volkswagen', 'Tiguan', 200, true, '../assest/imagenes/rental_car/vkwgn/vw-tiguan.jpg', 'family-van', 19);
+
+const car20 = new carForRent('Volkswagen', 'Vento', 100, true, '../assest/imagenes/rental_car/vkwgn/vw-vento.png', 'family-car', 20);
+
+
+const carsArray = [car1, car2, car3, car4, car5, car6, car7, car8, car9, car10, car11, car12, car13, car14, car15, car16, car17, car18, car19, car20];
+
+//VOLVER A CARGAR
+//localStorage.setItem('carsArray', carsArrayJSON);
+
+const carsArrayJSON = JSON.stringify(carsArray);
+const storedCarsArrayJSON = localStorage.getItem('carsArray');
+const storedCarsArray = JSON.parse(storedCarsArrayJSON);
+
+const container = document.getElementById('carsForRent');
+
+if(container) {
+    storedCarsArray.forEach(car => {
+        const carElement = document.createElement('div');
+        carElement.classList.add("carCont");
+        carElement.innerHTML = `<div class="img-cont"><img src="${car.img}"                        alt="${car.brand} ${car.model}"></div>
+                                <p><b> ${car.brand}</b></p>
+                                <p> ${car.model}</p>
+                                <p>Price: $${car.price} /Day</p>
+                                <p> ${car.availability ? 'Available' : 'Not Available'}</p>
+                                <button type="button" id="${car.id}" class="rentNow" onclick="rentNow(${car.id})">Rent Now</button>
+                                `;
+        container.appendChild(carElement);
+    });
+}
+
+function rentNow(carId){
+    if(sessionStatus === true){
+        sessionStorage.setItem('selectedCarId', carId);
+        window.location.href = './page/rentNow.html';
+        
+    } else {
+        Swal.fire({
+            icon: 'error',
+            title: 'Log in first',
+        });
+    }
+}
+
+
+
 function calculateTotalPrice() {
     const daysForRent = document.querySelector('.daysForRent').value;
     const selectedCarPrice = storedCarsArray.find(car => car.id === parseInt(sessionStorage.getItem('selectedCarId'))).price;
@@ -343,40 +356,53 @@ function calculateTotalPrice() {
 function goToPay() {
     const totalPrice = document.querySelector('.totalPrice').value;
     
+    
     const selectedCarId = sessionStorage.getItem('selectedCarId');
+    const selectedCar = storedCarsArray.find(car => car.id === parseInt(selectedCarId));
+
     if (selectedCar) {
+        
         console.log('Automóvil encontrado:', selectedCar);
 
         // Intenta actualizar la disponibilidad
         selectedCar.availability = false;
-
+        
+        // Actualiza el objeto correspondiente en storedCarsArray si es necesario
+        const index = storedCarsArray.findIndex(car => car.id === selectedCar.id);
+        if (index !== -1) {
+            storedCarsArray[index] = selectedCar;
+        }
+        
+        localStorage.setItem('carsArray', JSON.stringify(storedCarsArray));
+        // Guarda el estado actualizado en el almacenamiento local
+        sessionStorage.setItem('carsArray', JSON.stringify(storedCarsArray));
         // Imprime el array de autos almacenados después del cambio
         console.log('Array de autos después de la actualización:', storedCarsArray);
     } else {
-        console.error('Automóvil no encontrado con el ID:', selectedCarId);
+        console.log('Automóvil no encontrado con el ID:', selectedCarId);
     }
     Swal.fire({
-        title: 'Payment made successfully. Total paid is: ' + totalPrice,
-        customClass: {
-            title: 'my-title-class',
-        },
-        showConfirmButton: false,
-        timer: 2500,
-        didOpen: () => {
-            const title = Swal.getTitle();
-            const img = document.createElement('img');
-            img.src = '../assest/logos/WOTG-_2_.webp';
-            img.style.width = '100px';
-            img.style.marginBottom = '10px'; // Ajusta según sea necesario
+       title: 'Payment made successfully. Total paid is: ' + totalPrice,
+       customClass: {
+           title: 'my-title-class',
+       },
+       showConfirmButton: false,
+       timer: 2500,
+       didOpen: () => {
+           const title = Swal.getTitle();
+           const img = document.createElement('img');
+           img.src = '../assest/logos/WOTG-_2_.webp';
+           img.style.width = '100px';
+           img.style.marginBottom = '10px'; // Ajusta según sea necesario
     
-            title.parentNode.insertBefore(img, title);
+           title.parentNode.insertBefore(img, title);
         },
     });
 
 
 
     setTimeout(function() {
-        window.history.back();
+         window.history.back();
     }, 2500);
 }
 //FILTRO ASIDE
